b3e474eb421e409f21b9d9ad6f48179d005f5383
Jenna Moss <jenna.moss@hotmail.com>
2026-03-15 12:08:46 -0400

    creating constructor modelCar

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+//Link to model car:
+//https://modeltoycars.com/1962-volkswagen-classic-bus-ruby-kinsmart-5060d-1-32-scale-diecast-model-toy-car-brand-new-but-not-in-box/
+
+class modelCar {
+    constructor (make, model, year, colour, sku, upc, packaging, condition, scale, price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.colour = colour;
+        this.sku = sku;
+        this.upc = upc;
+        this.packaging = packaging;
+        this.condition = condition;
+        this.scale = scale;
+        this.price = price;
+    }
+}
